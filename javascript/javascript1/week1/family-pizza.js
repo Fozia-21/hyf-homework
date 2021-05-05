bcb7ec2982f86e03a020a3aa0fee3b7f9f2e144b
Fozia-21 <fozia.mehmood@yahoo.com>
2021-05-05 22:28:04 +0200

    Added code to family-pizza.js file

---
+++ javascript/javascript1/week1/family-pizza.js
@@ -0,0 +1,14 @@
+
+const pizzasPrice = 70;
+let amountOfPizzas = 3;
+let pizzaSize = 'Family Pizza';
+
+if (pizzaSize)
+{
+    totalPrice = 2 * pizzasPrice;//Family pizza price is double the price of normal pizza
+}
+
+console.log("New pizza order:"+" " +amountOfPizzas+" "+ pizzaSize+"."+"\n"+"Total cost of Order is:"+" "+ (3 * totalPrice));
+
+
+              
